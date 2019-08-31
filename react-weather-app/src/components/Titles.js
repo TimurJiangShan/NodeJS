82de1cb357209da1ec2e384acf791d5a0a8aa0c2
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2019-08-31 17:30:35 +1000

    React Weather App

---
+++ react-weather-app/src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Titles = () => (
+	<div>
+		<h1 className="title-container__title">Weather APP</h1>
+		<h3 className="title-container__subtitle">Search for conditions, temperature, and more...</h3>
+	</div>
+);
+
+export default Titles;
