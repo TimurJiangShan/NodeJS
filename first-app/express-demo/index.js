45acb363a0be3ecfc14851a2ae7c252b5d5ddf4e
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2020-11-20 22:27:02 +1100

    Express

--- first-app/express-demo/index.js
+++ first-app/express-demo/index.js
@@ -9,6 +9,10 @@ app.get('/api/courses', (req, res) => {
   res.send([1,2,3,4]);
 })
 
+app.get('/api/users', (req, res) => {
+  res.send([1,2,3,4]);
+})
+
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 })
