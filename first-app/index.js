b3dce83e99d349ba990f78997c4bf84d66ebab5b
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2020-11-20 21:31:17 +1100

    npm publish

--- first-app/index.js
+++ first-app/index.js
@@ -6,3 +6,15 @@ var _ = require('underscore');
 
 console.log(_.contains([1,3,2], 2))
 
+// npm list 列出安装的依赖
+// npm list --depth=0 列出第一层的依赖（像树一样）
+
+var keke = require('keke-lib-742345');
+console.log(keke.add(1,2));
+
+/**
+ * 1. npm adduser
+ * 2. npm login
+ * 3. npm publish(version 和 name一致)
+ * 
+*/
