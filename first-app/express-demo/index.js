0593313290f4eee99de92bec924a76a571c218de
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2020-11-21 15:25:08 +1100

    adding validation

--- first-app/express-demo/index.js
+++ first-app/express-demo/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const Joi = require('joi');
 const app = express();
+
+app.use(express.json()); // 使用中间件
 
 const courses = [
   { id: 1, name: 'course1'},
@@ -20,6 +23,38 @@ app.get('/api/courses/:id', (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
   if(!course) 
     res.status(200).send('The course with the given ID was not found');
+  res.send(course);
+
+})
+
+app.post('/api/courses', (req, res) => {
+
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  const options = {
+    abortEarly: false, // include all errors
+    allowUnknown: true, // ignore unknown props
+    stripUnknown: true // remove unknown props
+  };
+
+  const { error, value } = schema.validate(req.body, options);
+  if(error) {
+    res.status(200).send(error.details[0].message);
+    return;
+  }
+
+  if(!req.body.name || req.body.name.length < 3) {
+    // 400 Bad Request
+    res.status(400).send('Name is required and should be minium 3 characters');
+    return;
+  }
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  }
+  courses.push(course);
   res.send(course);
 
 })
