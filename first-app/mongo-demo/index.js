d7eeef56630526840ae1023123e329d886df035b
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2020-11-26 20:47:13 +1100

    Shcema

--- first-app/mongo-demo/index.js
+++ first-app/mongo-demo/index.js
@@ -3,3 +3,21 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/playground') // 返回一个promise
   .then(() => console.log('Connected to MongoDB...'))
   .catch(error => console.error("Could not connect to MongoDB...", error));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [ String ],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean
+});
+
+// Classes, Objects
+
+const Course = mongoose.model('Course', courseSchema);
+const course = new Course({
+  name: 'Node.js Course',
+  author: 'Jiangshan',
+  tags: ['node','backend'],
+  isPublished: true,
+});
